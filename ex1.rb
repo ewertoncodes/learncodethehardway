abd6e1fdd3e9edb59c9d56b9f0a3e798314b6b37
ewertonorg <tom.havoki@gmail.com>
2014-11-28 15:47:55 -0200

    adicionando os 5 primeiros exercicios do livro learn_ruby_the_hard_way

---
+++ ex1.rb
@@ -0,0 +1,8 @@
+puts "hello world!" 
+puts "hello world again."
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Printing."
+puts "I'd must rather you 'not'."
+puts 'I "said" do not touch this.'
+puts "" 

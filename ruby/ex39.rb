7cd2f31438cbf63afc8454e375284958536601c2
ewertonorg <tom.havoki@gmail.com>
2014-12-07 23:02:29 -0200

    adicionando exercicios

---
+++ ruby/ex39.rb
@@ -0,0 +1,50 @@
+states = {
+  'Oregon' => 'OR',
+  'Florida' => 'FL',
+  'Califorina' => 'CA',
+  'New York' => 'NY',
+  'Michigan' => 'MI'
+}
+
+cities = {
+  'CA' => 'San Francisco',
+  'MI' => 'Detroit',
+  'FL' =>  'Jacksonvilee'
+}
+
+cities['NY'] = 'New York'
+cities['OR'] = 'Portland'
+
+puts '-' * 10
+puts "Michigan's abbreviation is: #{states['Michigan']} "
+puts "Florida has: #{cities[states['Florida']]}"
+
+puts '-' * 10
+cities.each do |state, abbrev|
+  puts "#{state} is abbrevietd #{abbrev}"
+end
+
+puts '-' * 10
+cities.each do |abbrev, city|
+  puts "#{abbrev} has the city #{city}"
+end
+
+
+puts '-' * 10
+states.each do |state, abbrev|
+  city = cities[abbrev]
+  puts "#{state} is abbreviated #{abbrev} and has city #{city}"
+end
+
+puts '-' * 10
+
+state = states['Texas']
+
+if !state
+  puts "Sorry, no Texas."
+end
+
+
+city = cities['TX']
+city ||= 'Does Not Exist'
+puts "The city for the state 'TX' is: #{city}"

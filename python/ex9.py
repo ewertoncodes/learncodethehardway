9b170dd8dc6ba9ef9bfd0137557da3e24a34692c
ewertonorg <ewertonorg@gmail.com>
2015-05-21 14:22:00 -0300

    add ex6.py, ex7.py, ex8.py, ex9.py ex10.py

---
+++ python/ex9.py
@@ -0,0 +1,13 @@
+days = "Mon Tue Wed Thu Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print "Here are the days: ", days
+print "Here are the months: ", months
+
+print """
+There's something going on there.
+Width the  three double-quote.
+We'll be  able  to type as much as we like.
+Even4 lines if want, or 5,  or 6.
+"""
+
